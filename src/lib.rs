51c0d5abfd485f6e74da025238282602065a0c0a
Haoxi Tan <haoxi.tan@gmail.com>
2021-12-13 17:22:51 +1000

    added memory allocation stuff

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod interrupts;
 pub mod vga_buffer;
 pub mod cmd;
 pub mod strutils;
+pub mod memory;
 
 use lazy_static::lazy_static;
 use bootloader::{BootInfo,entry_point};
@@ -52,10 +53,31 @@ pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // x86_64::instructions::interrupts::int3(); 
 
     // println!("It did not crash!");
+
+
+    /////////////// memory stuff
+    use x86_64::VirtAddr;
+    use x86_64::structures::paging::Page;
+    use crate::memory::{self, BootInfoFrameAllocator};
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // map an unused (virtual) page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+    ///////////////
+
+
     print!("{}", crate::cmd::PROMPT);
 
     
-    loop {}
+    hlt_loop();
 }
 
 
